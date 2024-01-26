c8a914f2ffbc925e89d7ec19571d5e108c6d2f2c
krakovia <karola96@protonmail.com>
2024-01-26 16:36:43 +0100

    added hardCap, removed mint on deployment

--- src/base_erc20.sol
+++ src/base_erc20.sol
@@ -2,14 +2,15 @@
 pragma solidity 0.8.23;
 
 import { ERC20 }         from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { ERC20Capped } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
 import { Ownable }       from "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title  base_erc20
  * @notice Contract used to mint the base token for the bridge
  */
-contract base_erc20 is ERC20, Ownable(msg.sender) {
+contract base_erc20 is ERC20, Ownable, ERC20Capped {
     uint8 immutable private _decimals;
 
     /**
@@ -19,9 +20,12 @@ contract base_erc20 is ERC20, Ownable(msg.sender) {
      * @param _totalSupply total supply of the token
      * @param decimals_ decimals of the token
      */
-    constructor(string memory _name, string memory _symbol, uint _totalSupply, uint8 decimals_) ERC20(_name, _symbol) {
+    constructor(string memory _name, string memory _symbol, uint _totalSupply, uint8 decimals_) 
+    ERC20(_name, _symbol)
+    ERC20Capped(_totalSupply * 10 ** decimals_)
+    Ownable(msg.sender)
+    {
         _decimals = decimals_;
-        _mint(owner(), _totalSupply * 10 ** decimals_);
     }
 
     /**
@@ -70,5 +74,10 @@ contract base_erc20 is ERC20, Ownable(msg.sender) {
      */
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+
+    // override required by standard
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Capped) {
+        super._update(from, to, amount);
     }
 }
