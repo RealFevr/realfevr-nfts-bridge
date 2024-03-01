aea4a622fa147a89513cddfe9774e8a495d2f7f4
Tiago Bem <tiago.bem@gmail.com>
2024-03-01 17:10:46 +0000

    RFB-10 | Typos and Inconsistencies V2

--- src/ERC20BridgeImpl.sol
+++ src/ERC20BridgeImpl.sol
@@ -393,7 +393,7 @@ contract ERC20BridgeImpl is AccessControlUpgradeable, ReentrancyGuardUpgradeable
     }
 
     function createNewToken(string memory _name, string memory _symbol, uint _totalSupply, uint8 _decimals) external returns(address) {
-        // only operator or bridge can call this
+        // only DEFAULT_ADMIN_ROLE or OPERATOR can call this
         if(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender) && !hasRole(OPERATOR, msg.sender)) revert NotAuthorized();
         return(address(new base_erc20(_name, _symbol, _totalSupply, _decimals)));
     }
